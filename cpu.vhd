747eca13cd696d922d414a153336082fd792c613
Yangzj13 <yangzj13@mails.tsinghua.edu.cn>
2016-11-21 20:28:09 +0800

    First commit

---
+++ cpu.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:23:58 11/21/2016 
+-- Design Name: 
+-- Module Name:    cpu - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cpu is
+    Port ( rst : in  STD_LOGIC;
+           clk : in  STD_LOGIC);
+end cpu;
+
+architecture Behavioral of cpu is
+
+begin
+
+
+end Behavioral;
+
